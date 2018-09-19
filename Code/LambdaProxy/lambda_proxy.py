000205dea3653fc5321045cbe04c08109d492294
sushicat3 <swetal.bhatt@gmail.com>
2018-09-19 17:42:09 -0400

    tries connecting to psql rds

--- Code/LambdaProxy/lambda_proxy.py
+++ Code/LambdaProxy/lambda_proxy.py
@@ -5,6 +5,19 @@ import os
 sys.path.append('./lib')
 import psycopg2
 
+class DatabaseConnection:
+	def __init__(self):
+		try:
+			self.connection = psycopg2.connect("dbname='musicbrainz' user='musicbrainz' host='musicbrainz.cw2va02rdk2z.us-east-1.rds.amazonaws.com' password='musicbrainz' port='5432'")
+			self.connection.autocommit = True
+			self.cursor = self.connection.cursor()
+		except:
+			print('cannot connect to database')
+
+	def get(self):
+		select_command = 'select * from users'
+		self.cursor.execute(select_command)
+
 def request_handler(event, context):
 	response = {}
 	response['isBase64Encoded'] = False
@@ -19,5 +32,8 @@ def request_handler(event, context):
 	body['album'] = 'Abbey Road'
 
 	response['body'] = json.dumps(body)
+
+	data_conn = DatabaseConnection()
+	print(data_conn.get())
 
 	return response
