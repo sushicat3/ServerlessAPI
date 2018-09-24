b403352c2c7ef7f9ad0cc91ed90f7824898ba2c2
sushicat3 <swetal.bhatt@gmail.com>
2018-09-24 17:49:32 -0400

    updates database stack. updates ec2 stack to use s3 instead of git repo

--- EC2Arch/deploy.sh
+++ EC2Arch/deploy.sh
@@ -10,6 +10,15 @@ if [ -z "$STACK_NAME" ]; then
   exit 1
 fi
 
+# copy app to s3
+if [ -f package.zip ]; then
+	rm package.zip
+fi
+
+zip -r package.zip app.py lib
+
+aws s3 cp package.zip s3://server-fwk/app.py
+
 # https://docs.aws.amazon.com/cli/latest/reference/cloudformation/create-stack.html
 aws cloudformation create-stack \
 	--stack-name $STACK_NAME \
