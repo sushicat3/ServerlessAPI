e659d31d910ff9850830945d043a98a7df0fdd74
sushicat3 <swetal.bhatt@gmail.com>
2018-09-24 16:59:52 -0400

    adds postgres db automation. updates user-data in ec2 stack

---
+++ Datastores/Postgres/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+STACK_NAME=$1
+
+USE_MSG="Usage: deploy.sh BUCKET STACK_NAME"
+
+if [ -z "$STACK_NAME" ]; then
+  echo "Missing STACK_NAME"
+  echo $USE_MSG
+  exit 1
+fi
+
+# https://docs.aws.amazon.com/cli/latest/reference/cloudformation/create-stack.html
+aws cloudformation create-stack \
+	--stack-name $STACK_NAME \
+	--template-body file://postgresRDS.yml
+
